75f02fda3de78a0ddbe34ef8c81620bb4d7d1b19
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-11-24 19:57:49 +0530

    debug a error

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -3,8 +3,11 @@ import { Button } from "./ui/button";
 import { Bookmark } from "lucide-react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
+import { useNavigate } from "react-router-dom";
 
 const Job = () => {
+  const navigate = useNavigate();
+  const jobid = "rbjj";
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
       <div className="flex items-center justify-between">
@@ -46,7 +49,7 @@ const Job = () => {
       </div>
       <div className="flex items-center gap-4 mt-4">
         <Button
-          //   onClick={() => navigate(`/description/${job?._id}`)}
+          onClick={() => navigate(`/description/${jobid}`)}
           variant="outline"
         >
           Details
