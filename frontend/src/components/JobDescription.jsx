950d823d54126eecfb82761abf76c26d30f0fd30
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-12-17 20:51:04 +0530

    debug a error

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -2,27 +2,28 @@ import React, { useEffect } from "react";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 import { setSingleJob } from "@/redux/jobSlice";
+import axios from "axios";
 import { JOB_API_END_POINT } from "@/utils/constant";
 import { useDispatch, useSelector } from "react-redux";
 
 const JobDescription = () => {
   // isApplied = true;
-  const { singleJob } = useSelector((store) => store.job);
-  const { user } = useSelector((store) => store.auth);
   const params = useParams();
   const jobId = params.id;
+  const { singleJob } = useSelector((store) => store.job);
+  const { user } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
-  const isApplied = singleJob?.applications?.includes(user?._id);
+
   useEffect(() => {
     const fetchSingleJob = async () => {
       try {
         const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, {
           withCredentials: true,
         });
+
         if (res.data.success) {
-          dispatch(setSingleJob(res.data.jobs));
+          dispatch(setSingleJob(res.data.job));
         }
       } catch (error) {
         console.log(error);
@@ -38,7 +39,7 @@ const JobDescription = () => {
           <h1 className="font-bold text-xl">{singleJob?.title}</h1>
           <div className="flex items-center gap-2 mt-4">
             <Badge className={"text-blue-700 font-bold"} variant="ghost">
-              {singleJob?.postion} Positions
+              {singleJob?.position}
             </Badge>
             <Badge className={"text-[#F83002] font-bold"} variant="ghost">
               {singleJob?.jobType}
@@ -48,9 +49,18 @@ const JobDescription = () => {
             </Badge>
           </div>
         </div>
-        <Button disabled={isApplied}>
-          {isApplied ? "Applied" : "Apply Now"}
-        </Button>
+        {/* <Button
+          //   onClick={isApplied ? null : applyJobHandler}
+          //   disabled={isApplied}
+          className={`rounded-lg ${
+            isApplied
+              ? "bg-gray-600 cursor-not-allowed"
+              : "bg-[#7209b7] hover:bg-[#5f32ad]"
+          }`}
+        >
+          {isApplied ? "Already Applied" : "Apply Now"}
+        </Button> */}
+        <Button> Apply now</Button>
       </div>
       <h1 className="border-b-2 border-b-gray-300 font-medium py-4">
         Job Description
@@ -88,15 +98,11 @@ const JobDescription = () => {
         </h1>
         <h1 className="font-bold my-1">
           Total Applicants:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.applications?.length}
-          </span>
+          <span className="pl-4 font-normal text-gray-800">2</span>
         </h1>
         <h1 className="font-bold my-1">
           Posted Date:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.createdAt.split("T")[0]}
-          </span>
+          <span className="pl-4 font-normal text-gray-800">12-2-330</span>
         </h1>
       </div>
     </div>
