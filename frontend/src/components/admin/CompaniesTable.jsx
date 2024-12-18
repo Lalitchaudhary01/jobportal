769b5f802ed6a7d43648c847cecf54e301959dd9
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-12-18 11:35:39 +0530

    building admin dashboard

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Edit2, MoreHorizontal } from "lucide-react";
 
 const CompaniesTable = () => {
   return (
     <div>
-      tabel
+      <Table>
+        <TableCaption>A list of your recent registered companies</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Logo</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <Avatar>
+                <AvatarImage
+                  src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"
+                  alt="@shadcn"
+                />
+              </Avatar>
+            </TableCell>
+            <TableCell>Shadcn</TableCell>
+            <TableCell>9.29.2020</TableCell>
+            <TableCell className="text-right">
+              <Popover>
+                <PopoverTrigger>
+                  <MoreHorizontal />
+                </PopoverTrigger>
+                <PopoverContent className="w-32">
+                  <div className="flex items-center gap-2 w-fit cursor-pointer">
+                    <Edit2 className="w-4" />
+                    <span>Edit</span>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
-export default CompaniesTable
+export default CompaniesTable;
